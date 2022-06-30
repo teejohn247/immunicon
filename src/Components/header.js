fec1a75204852ad8e9de67ab99e9f94ecf02ad27
teejohn247 <teejoh247@gmail.com>
2022-06-30 19:11:01 +0100

    fixed language

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,11 +1,12 @@
 import React, { useState, } from 'react';
 
-import { Anchor, Drawer, Button, Image } from 'antd';
+import { Anchor, Drawer, Button, Image, Select } from 'antd';
 
 import { MenuOutlined } from '@ant-design/icons';
 
 import sed from '../assets/logo.png';
 
+const { Option } = Select;
 
 
 
@@ -34,6 +35,22 @@ function AppHeader() {
             <Link href="/" title="Home" />
             <Link href="/" title="Whitepaper" />
             <Link href="/about" title="More" />
+            <Select
+              defaultValue="Language"
+              style={{
+                padding: "10px",
+
+              }}
+            >
+              <Option value="English">English</Option>
+              <Option value="french">Français</Option>
+              <Option value="Deutsch">Deutsch</Option>
+              <Option value="Türkçe">Türkçe</Option>
+              <Option value="Spanish">Spanish</Option>
+              <Option value="Italian">Italian</Option>
+              <Option value="Arabic">Arabic</Option>
+              <Option value="Japanese">Japanese</Option>
+            </Select>
             <Button style={{width: "150px",height: "60px", borderRadius: "4px"}} className="txt" title="AUDIT">AUDIT</Button>
 
           </Anchor>
@@ -60,6 +77,23 @@ function AppHeader() {
               <Link href="/" title="Home" /><span className="ant-drawer-header ant-drawer-header-close-only"></span>
               <Link href="/" title="Whitepaper" />
               <Link href="/about" title="More" />
+              <Select
+              defaultValue="Language"
+              style={{
+                padding: "10px",
+
+              }}
+            >
+              <Option value="English">English</Option>
+              <Option value="french">Français</Option>
+              <Option value="Deutsch">Deutsch</Option>
+              <Option value="Türkçe">Türkçe</Option>
+              <Option value="Spanish">Spanish</Option>
+              <Option value="Italian">Italian</Option>
+              <Option value="Arabic">Arabic</Option>
+              <Option value="Japanese">Japanese</Option>
+            </Select>
+              
               <Button style={{width: "150px",height: "60px", borderRadius: "4px", margin: "4px 0 6px 16px"}} className="txt" title="AUDIT">AUDIT</Button>
 
             </Anchor>
